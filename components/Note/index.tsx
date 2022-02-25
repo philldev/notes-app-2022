1b9eab3f3efc0096a09b119db9a804f198b596e6
deddywolley <deddywolley@gmail.com>
2022-02-25 12:42:48 +0800

    add sanitize html

--- components/Note/index.tsx
+++ components/Note/index.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react'
+import { generateId } from '../../utils'
 import { NoteContentEditable } from './note-content-editable'
 
+interface Note {
+	id: string
+	title: string
+	content: string
+}
+
 const Note = () => {
+	const [note, setNote] = useState<Note>({
+		id: generateId(),
+		title: '',
+		content: '',
+	})
 	return (
 		<div className='flex-1 p-4'>
 			<div className='mb-4'>
 				<input
+					value={note.title}
+					onChange={(event) =>
+						setNote({
+							...note,
+							title: event.target.value,
+						})
+					}
 					className='w-full h-full text-2xl font-bold bg-slate-100 focus:outline-none'
 					placeholder='Title'
 				/>
 			</div>
 			<div>
-				<NoteContentEditable html='' onChange={() => {}} />
+				<NoteContentEditable
+					html={note.content}
+					onChange={(html) => {
+						setNote({
+							...note,
+							content: html,
+						})
+					}}
+				/>
 			</div>
 		</div>
 	)

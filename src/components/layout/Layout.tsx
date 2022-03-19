b21a934de56306c5f18f5b3353d6c98dad197ef4
deddywolley <deddywolley@gmail.com>
2022-03-19 11:16:57 +0800

    feat: added note list

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
+  return <div className='min-h-screen w-full bg-slate-100'>{children}</div>;
 }

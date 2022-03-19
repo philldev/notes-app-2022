52ce6179e907660aab4f1b2c40909668475ca923
deddywolley <deddywolley@gmail.com>
2022-03-19 12:02:20 +0800

    feat: update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FiEdit } from 'react-icons/fi';
 
 import Layout from '@/components/layout/Layout';
 import NoteList from '@/components/note-list/note-list';
@@ -9,6 +10,14 @@ export default function HomePage() {
     <Layout>
       <Seo />
       <main>
+        <div className='flex items-center justify-between p-2'>
+          <h1 className='text-xl font-normal uppercase'>Note App</h1>
+          <div className=''>
+            <button className='flex h-10 w-10 items-center justify-center'>
+              <FiEdit className='h-4 w-4' />
+            </button>
+          </div>
+        </div>
         <NoteList />
       </main>
     </Layout>

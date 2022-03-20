2f0347d7b9981d16cc05b596524e0960b3631c1a
deddywolley <deddywolley@gmail.com>
2022-03-20 13:47:17 +0800

    feat: update test

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,7 +1,5 @@
 import { render } from '@testing-library/react';
 
-import { NotesProvider } from '@/lib/notes/notes';
-
 import HomePage from '@/pages';
 
 /** Mock Seo's useRouter */
@@ -15,11 +13,7 @@ jest.mock('next/router', () => ({
 
 describe('Index Page', () => {
   it('renders index page', async () => {
-    const { container } = render(
-      <NotesProvider>
-        <HomePage />
-      </NotesProvider>
-    );
+    const { container } = render(<HomePage />);
 
     expect(container.firstChild?.hasChildNodes()).toBeTruthy();
   });
